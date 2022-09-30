834109273f616519370c09f73e80da3dda48eb86
Kevin Dang <kevinthedang_1@outlook.com>
2022-09-29 20:59:04 -0700

    added movement and jumping

--- 2d project/Assets/Scripts/PlayerMovement.cs
+++ 2d project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,33 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController2D character;
+
+    public float speed = 50f;
+
+    private float horizontal_movement = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal_movement = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    void FixedUpdate()
+    {
+        character.Move(horizontal_movement * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
